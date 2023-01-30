32ca7cdeda5eb302482557613b856b9d47db8894
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2023-01-29 22:27:40 -0300

    melhorando layout  adicionando JavaScript ao cashback container

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+
+/* CARROSSEL JS */
 const swiper = new Swiper('.top-swiper', {
     // Optional parameters
     direction: 'horizontal',
@@ -29,3 +31,47 @@ const swiper = new Swiper('.top-swiper', {
       prevEl: '.mid-btn-prev',
     },
   });
+
+
+/* SLIDER-preço JS */  
+
+let cashVal = document.getElementById('cashback-value');
+let cost = document.getElementById('cost');
+let cost2 = document.getElementById('cost2');
+let slider = document.getElementById('price-slider');
+let toggle = document.getElementById('billing');
+let percentage = ["5%", "10%", "20%", "25%", "30%"];
+let perMonth = [8, 12, 16, 24, 36];
+let isYearly = false;
+
+slider.addEventListener('input', () => {
+    updateValue();
+    cashVal.innerHTML = percentage[slider.value]
+
+    let value = slider.value *25
+    
+    slider.style.background = `linear-gradient(to right,
+        hsl(191, 48%, 55%) 0%,
+        hsl(191, 48%, 55%) ${value}%,
+        hsl(224, 65%, 95%) 0%,
+        hsl(224, 65%, 95%) 100%)`
+});
+
+toggle.addEventListener('change', () => {
+    if (isYearly == false) {
+        isYearly = true
+    } else {
+        isYearly = false
+    }
+    updateValue();
+});
+
+function updateValue () {
+    if (isYearly) {
+        cost.innerHTML = perMonth[slider.value] * .75
+        cost2.innerHTML = perMonth[slider.value] * .75
+    } else {
+        cost.innerHTML = perMonth[slider.value]
+        cost2.innerHTML = perMonth[slider.value]
+    }
+};
